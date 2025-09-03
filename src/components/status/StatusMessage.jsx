2df29f3259c824967dae3bdd2c6b7d0dbb1a53ee
mikewanjero <mikewanj@gmail.com>
2025-09-03 09:21:24 +0300

    {REFACTOR} fonts

--- src/components/status/StatusMessage.jsx
+++ src/components/status/StatusMessage.jsx
@@ -3,15 +3,15 @@ import { PartyPopper } from "lucide-react";
 
 export default function StatusMessage() {
   return (
-    <div className="flex flex-col items-center justify-center text-center space-y-5 mb-16">
+    <div className="flex flex-col items-center justify-center text-center space-y-5">
       {/* Header */}
       <div className="flex items-center space-x-2">
-        <h2 className="text-2xl font-bold text-gray-800">Good News!</h2>
+        <h3 className="text-base font-bold text-gray-800">Good News!</h3>
         <PartyPopper className="w-7 h-7 text-yellow-500" />
       </div>
 
       {/* Message */}
-      <p className="text-gray-700 text-lg max-w-md">
+      <p className="text-gray-700 text-sm max-w-md">
         Congratulations! Your order just left dispatch and it is officially
         coming your way.
       </p>
