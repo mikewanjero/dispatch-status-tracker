40b7094dbda62b4e3f2d258e3a817888ca23f0a5
mikewanjero <mikewanj@gmail.com>
2025-09-03 09:20:31 +0300

    {FEAT} Buttons

--- src/components/status/StatusActions.jsx
+++ src/components/status/StatusActions.jsx
@@ -4,11 +4,11 @@ export default function StatusActions() {
   return (
     <>
       <div className="flex flex-col space-y-4">
-        <Button variant="default" size="xl">
+        <Button variant="default" size="sm">
           Contact Driver
         </Button>
-        <Button variant="default" size="xl">
-          Contact Support
+        <Button variant="default" size="sm">
+          Contact Customer Care
         </Button>
       </div>
     </>
