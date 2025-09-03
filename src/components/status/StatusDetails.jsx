fb65d417f8d6be855466137c7b195544fca2234d
mikewanjero <mikewanj@gmail.com>
2025-09-03 09:21:05 +0300

    {REFACTOR} fonts, alignment

--- src/components/status/StatusDetails.jsx
+++ src/components/status/StatusDetails.jsx
@@ -3,38 +3,43 @@ import { Label } from "@/components/ui/label";
 export default function StatusDetails() {
   return (
     <>
-      <section className="p-4 flex flex-col space-y-4 text-left">
-        <section className="flex flex-col items-start gap-1">
-          <Label className="text-lg font-medium">Tracking ID:</Label>
-          <Label className="text-lg font-medium text-muted uppercase">
+      <section className="flex flex-col space-y-4 text-left">
+
+        <section className="flex flex-col justify-center items-center">
+          <Label className= "uppercase">Your Order</Label>
+        </section>
+
+        <section className="flex flex-row justify-between items-start gap-1">
+          <Label className="text-xs font-medium">Tracking ID:</Label>
+          <Label className="text-xs font-medium text-muted uppercase">
             #DSP-12345
           </Label>
         </section>
 
-        <section className="flex flex-col items-start gap-1">
-          <Label className="text-lg font-medium">Invoice No:</Label>
-          <Label className="text-lg font-medium text-muted uppercase">
+        <section className="flex flex-row justify-between items-start gap-1">
+          <Label className="text-xs font-medium">Invoice No:</Label>
+          <Label className="text-xs font-medium text-muted uppercase">
             W1_20022693
           </Label>
         </section>
 
-        <section className="flex flex-col items-start gap-1">
-          <Label className="text-lg font-medium">Order Date :</Label>
-          <Label className="text-lg font-medium text-muted uppercase">
-            2023-03-15
+        <section className="flex flex-row justify-between items-start gap-1">
+          <Label className="text-xs font-medium">Order Date :</Label>
+          <Label className="text-xs font-medium text-muted uppercase">
+            2025-08-23
           </Label>
         </section>
 
-        <section className="flex flex-col items-start gap-1">
-          <Label className="text-lg font-medium">Delivery Address:</Label>
-          <Label className="text-lg font-medium text-muted uppercase">
+        <section className="flex flex-row justify-between items-start gap-1">
+          <Label className="text-xs font-medium">Delivery Address:</Label>
+          <Label className="text-xs font-medium text-muted uppercase">
             Mapera Court - Lang’ata Rd
           </Label>
         </section>
 
-        <section className="flex flex-col items-start gap-1">
-          <Label className="text-lg font-medium">Driver Assigned:</Label>
-          <Label className="text-lg font-medium text-muted uppercase">
+        <section className="flex flex-row justify-between items-start gap-1">
+          <Label className="text-xs font-medium">Driver Assigned:</Label>
+          <Label className="text-xs font-medium text-muted uppercase">
             PETER TANUI - KCA866H
           </Label>
         </section>
