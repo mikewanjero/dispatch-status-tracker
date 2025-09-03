ab4e253fa03ec8c74598ace9bb583ff6fd5d5577
mikewanjero <mikewanj@gmail.com>
2025-09-03 09:22:32 +0300

    {FEAT} stage `structure`

--- src/components/status/StatusTimeline.jsx
+++ src/components/status/StatusTimeline.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { stages } from '../../features/status-progress/stageData'
+import ProcessStage from '../../features/status-progress/ProcessStage'
 
 export default function StatusTimeline() {
   return (
-    <div className='flex flex-row mx-4 justify-between items-center'>
-      
+    <div className='flex flex-row mx-auto justify-between items-center'>
+      {stages.map((stage) => (
+          <ProcessStage key={stage.label} stage={stage} />
+        ))}
     </div>
   )
 }
