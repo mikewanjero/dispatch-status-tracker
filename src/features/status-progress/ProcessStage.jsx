d2e6493892845b46eb8299b535887d700e6ae1b1
mikewanjero <mikewanj@gmail.com>
2025-10-16 18:28:50 +0300

    {FEAT} sorting values to ensure consistent stage order

--- src/features/status-progress/ProcessStage.jsx
+++ src/features/status-progress/ProcessStage.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
-import { iconMap } from '../ui/icons'
+import { iconMap, statusColor } from '../ui/icons'
 
 export default function ProcessStage({ stage }) {
-    const Icon = iconMap[stage.label];
-    const processColor = iconMap[stage.status];
+    const Icon = iconMap[stage.stage?.toUpperCase()] 
+    const processColor = statusColor[stage.status?.toLowerCase()] || "text-gray-400";
+
+    const timestamp = stage.timestamp
+        ? new Date(stage.timestamp).toLocaleString("en-GB", {
+                day: "2-digit",
+                month: "short",
+                hour: "2-digit",
+                minute: "2-digit",
+            })
+        : "Pending";
 
     return (
         <div className='flex flex-col items-center gap-y-1'>
-            <span className='mt-2 text-xs font-medium'>{stage.label}</span>
+            {/* Stage Name */}
+            <span className='mt-2 text-xs font-medium'>{stage.stage}</span>
+            {/* Stage Icon*/}
             <Icon size={20} className={processColor} />
-            <section className='flex flex-row gap-x-1'>
-                <span className='text-xs text-gray-500'>{stage.date || 'Pending'}</span>
-                <span className='text-xs text-gray-500'>{stage.time}</span>
+            {/* Timestamp */}
+            <section className="flex flex-row gap-x-1 text-xs text-gray-500">
+                {timestamp !== "Pending" ? (
+                    <span>{timestamp}</span>
+                ) : (
+                    <span className="italic text-gray-400">Pending</span>
+                )}
             </section>
         </div>
     )
