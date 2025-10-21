8b66bb87ea989281ad293a61284a7a0f0bdbe52a
mikewanjero <mikewanj@gmail.com>
2025-10-21 13:29:25 +0300

    {FIX} new url

--- src/features/status/statusprogressAPI.js
+++ src/features/status/statusprogressAPI.js
@@ -3,8 +3,8 @@ import { apiClient } from "@/app/api-client";
 export const statusProgressApi = apiClient.injectEndpoints({
   endpoints: (builder) => ({
     getStatusTracker: builder.query({
-      query: (docNo) => ({
-        url: `/api/v2/status-tracker/${docNo}`,
+      query: (token) => ({
+        url: `/api/v2/status-tracker/${token}`,
         method: "GET",
       }),
       providesTags: ["status_tracker"],
